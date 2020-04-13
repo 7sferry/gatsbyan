ff7da17c45741ebc70400b8b19e1f52749ea782a
MR Ferry <ferry.suhandri@pji-solutions.com>
2020-04-14 01:33:00 +0700

    feat: change tracedSVG effect to blur up

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -19,7 +19,6 @@ const useSiteMetadata = () => {
                         srcSetWebp
                         srcWebp
                         base64
-                        tracedSVG
                     }
                 }
             }
