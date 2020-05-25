b5f4a8131ed809af5acb2c80da68ad14d76eacb2
MR Ferry <ferry.suhandri@pji-solutions.com>
2020-05-25 18:43:23 +0700

    completely remove google font plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,16 +116,6 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       `Raleway`,
-    //       `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-    //     ],
-    //     display: 'swap'
-    //   },
-    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
