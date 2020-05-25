859ee17888358790f8dc8e5e85a587feed67a01a
MR Ferry <ferry.suhandri@pji-solutions.com>
2020-05-25 18:16:12 +0700

    test enable font

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,16 +116,16 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       `Raleway`,
-    //       `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-    //     ],
-    //     display: 'swap'
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Raleway`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
