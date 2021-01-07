62ecba1355316c948b06bb39ff6f813bb253527c
MR Ferry <ferry.suhandri@pji-solutions.com>
2021-01-07 14:38:35 +0700

    fix: sensitive tokens to env

---
+++ gatsby-config.js
@@ -0,0 +1,206 @@
+/************************
+ * Made by [MR Ferry™]  *
+ * on January 2021      *
+ ************************/
+
+"use strict";
+require("dotenv").config();
+const siteConfig = require("./config");
+const contentfulConfig = {
+  spaceId: process.env.SPACEID,
+  accessToken: process.env.TOKEN,
+};
+const queries = require("./src/utils/Algolia");
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: siteConfig.url,
+    title: siteConfig.title,
+    tagline: siteConfig.tagline,
+    description: siteConfig.description,
+    author: siteConfig.author.name,
+    realName: siteConfig.author.realName,
+    copyright: siteConfig.copyright,
+    contacts: {
+      linkedin: siteConfig.author.contacts.linkedin,
+      github: siteConfig.author.contacts.github,
+      crystal: siteConfig.author.contacts.crystal,
+      blogger: siteConfig.author.contacts.blogger,
+      resume: siteConfig.author.contacts.resume,
+      facebook: siteConfig.author.contacts.facebook,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark-plaintext`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: true,
+        stripMetadata: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        sitemapSize: 5000,
+      },
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images-zoom`,
+            options: {
+              margin: 0,
+              scrollOffset: 0,
+              background: "transparent",
+              zIndex: 0,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images-contentful`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1200,
+              linkImagesToOriginal: false,
+              withWebp: true,
+              showCaptions: true,
+              backgroundColor: "transparent",
+              loading: "lazy",
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteConfig.author.realName,
+        short_name: siteConfig.author.name,
+        description: siteConfig.description,
+        lang: `id`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#3948DF`,
+        display: `minimal-ui`,
+        crossOrigin: `use-credentials`,
+        icon_options: {
+          // For all the options available, please see:
+          // https://developer.mozilla.org/en-US/docs/Web/Manifest
+          // https://w3c.github.io/manifest/#purpose-member
+          purpose: `maskable`,
+        },
+        icon: `src/images/avatar.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics-reporter`,
+      options: {
+        email: process.env.ANALYTICS_EMAIL,
+        privateKey: process.env.ANALYTICS_PRIVATE_KEY,
+        viewId: process.env.ANALYTICS_VIEW_ID,
+        // startDate: `30daysAgo`,
+        endDate: `today`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-165368793-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        pageTransitionDelay: 250,
+        exclude: ["/tags/**", "/search", "/archive", "/404", "/404.html"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "OPT-P5SQMN6",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "xgp72D0ZQaKYe3dWfy0L0Q",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        // defer: false,
+        // Any additional optional fields
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        // printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        ignore: ['/ignored'], // Ignore files/folders
+      }
+    },
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "GTM-TXJJ82Z",
+    //
+    //     // Include GTM in development.
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
+    //
+    //     // datalayer to be set before GTM is loaded
+    //     // should be an object or a function that is executed in the browser
+    //     // Defaults to null
+    //     defaultDataLayer: { platform: "gatsby" },
+    //
+    //     // Specify optional GTM environment details.
+    //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+    //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+    //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+    //   },
+    // },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`,
+    // {
+    //   resolve: "gatsby-plugin-algolia",
+    //   options: {
+    //     appId: process.env.GATSBY_ALGOLIA_APP_ID,
+    //     apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
+    //     indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+    //     queries,
+    //     enablePartialUpdates: true,
+    //     chunkSize: 10000,
+    //   },
+    // },
+  ],
+};
