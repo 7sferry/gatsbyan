c83bce4db1c362d2ace05c42eae6e86d16ebb029
MR Ferry <ferry.suhandri@pji-solutions.com>
2021-02-07 22:27:45 +0700

    prettier

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -10,10 +10,17 @@ export default class Comment extends Component {
   render() {
     return (
       <div style={{ position: "relative" }}>
-        <p style={{ position: "absolute", left: '50%' }}>LOADING...</p>
+        <p style={{ position: "absolute", left: "50%" }}>LOADING...</p>
         <div style={{ zIndex: 9 }}>
           <FacebookProvider appId="1365740643629290">
-            <Comments style={{zIndex: 9}} onload={console.log("koklko")}  href={this.props.href} orderBy="reverse_time" mobile={true} width="100%" colorScheme="dark" />
+            <Comments
+              style={{ zIndex: 9 }}
+              href={this.props.href}
+              orderBy="reverse_time"
+              mobile={true}
+              width="100%"
+              colorScheme="dark"
+            />
           </FacebookProvider>
         </div>
       </div>
