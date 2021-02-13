e7fc63ec651d62e93b2ef311a2eb1f0eff7ae818
MR Ferry <ferry.suhandri@pji-solutions.com>
2021-02-13 12:44:58 +0700

    cleaning code

--- src/components/sidebar/RightSidebar.js
+++ src/components/sidebar/RightSidebar.js
@@ -10,14 +10,12 @@ import FeaturedPage from "./FeaturedPage";
 
 const RightSidebar = () => {
   return (
-    <>
-      <div className="sidebar-main ">
-        <div className="second-header">Most Viewed</div>
-        {AnalyticsPage}
-        <div className="second-header">Featured Post</div>
-        {FeaturedPage}
-      </div>
-    </>
+    <div className="sidebar-main ">
+      <div className="second-header">Most Viewed</div>
+      <AnalyticsPage />
+      <div className="second-header">Featured Post</div>
+      <FeaturedPage />
+    </div>
   );
 };
 
