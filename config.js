3f4d4a6d4d232ee9d04780a15f0f2f35ebb0af86
ferr <ferry@advotics.com>
2021-10-21 10:27:54 +0700

    feat: update description

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   repo: "7sferry/gatsbyan",
   title: "MR Ferry",
   tagline: "An ISTJ, Type 5, Engineer, Gamer, and Thriller-Movies-Lover",
-  description: `Blog by Ferry Suhandri. ISTJ, Type 5 Enneagram, Engineer dari Solok, berpengalaman di Industri Teknologi. Berisi pandangan pribadi, Java, JS, Spring dan pemrograman dalam bahasa indonesia.`,
+  description: `Blog by Ferry Suhandri. an ISTJ, Type 5 Enneagram, Engineer dari Solok, berpengalaman di Industri Teknologi. Berisi pandangan pribadi, pemrograman dan sejenisnya dalam bahasa indonesia.`,
   copyright: `© ${new Date().getFullYear()} Ferry Suhandri.`,
   author: {
     name: "Ferry S",
