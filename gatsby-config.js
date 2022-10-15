e0a03abd09059ff3975df798f5c02fab47ecaee9
MR Ferry <ferry.suhandri@pji-solutions.com>
2022-10-15 22:26:33 +0700

    feat: add ga v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -179,7 +179,7 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           process.env.ANALYTICS_GA4, // Google Analytics / GA
-          process.env.ANALYTICS_MEASUREMENT_ID, // Google Analytics / GA
+          // process.env.ANALYTICS_MEASUREMENT_ID, // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
