e690b3168c5984c8a6aa0a404792c2fcc40c6db9
MR Ferry <ferry.suhandri@pji-solutions.com>
2022-10-16 10:13:10 +0700

    feat: re-add ga4

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,38 +173,38 @@ module.exports = {
     //     // cookieDomain: "example.com",
     //   },
     // },
-    // {
-    //   resolve: `gatsby-plugin-google-gtag`,
-    //   options: {
-    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
-    //     trackingIds: [
-    //       // process.env.ANALYTICS_GA4, // Google Analytics / GA
-    //       process.env.ANALYTICS_MEASUREMENT_ID, // Google Analytics / GA
-    //       // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-    //       // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-    //     ],
-    //     // This object gets passed directly to the gtag config command
-    //     // This config will be shared across all trackingIds
-    //     gtagConfig: {
-    //       // optimize_id: process.env.ANALYTICS_OPTIMIZE_ID,
-    //       anonymize_ip: true,
-    //       // cookie_expires: 0,
-    //     },
-    //     // This object is used for configuration specific to this plugin
-    //     pluginConfig: {
-    //       // Puts tracking script in the head instead of the body
-    //       // head: true,
-    //       // Setting this parameter is also optional
-    //       // respectDNT: true,
-    //       // Avoids sending pageview hits from custom paths
-    //       exclude: exclude,
-    //       pageTransitionDelay: 250,
-    //       defer: true,
-    //       // Defaults to https://www.googletagmanager.com
-    //       // origin: "https://www.googletagmanager.com",
-    //     },
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          // process.env.ANALYTICS_GA4, // Google Analytics / GA
+          process.env.ANALYTICS_MEASUREMENT_ID, // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: process.env.ANALYTICS_OPTIMIZE_ID,
+          anonymize_ip: true,
+          // cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          // head: true,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: exclude,
+          pageTransitionDelay: 250,
+          defer: true,
+          // Defaults to https://www.googletagmanager.com
+          // origin: "https://www.googletagmanager.com",
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
