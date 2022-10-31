4424e43eab47ddd914ae9e9782ac558901422b56
ferr <ferry@advotics.com>
2022-10-31 19:02:04 +0700

    refactor: graphql

--- src/templates/index.js
+++ src/templates/index.js
@@ -14,17 +14,17 @@ import { getPlurals, getPublishDate, getPostTags } from "../utils/GatsbyanUtils"
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-class IndexPage extends React.Component{
-  render(){
+class IndexPage extends React.Component {
+  render() {
     const { allContentfulBlogPost: contents } = this.props.data;
-    const { edges: posts, pageInfo } = contents;
+    const { nodes: posts, pageInfo } = contents;
     const kebabTag = this.props.pageContext.kebabTag;
     const paginationUrl = kebabTag ? `/tags/${kebabTag}` : `/page`;
     const title = () => {
-      if(paginationUrl.startsWith("/tags")){
+      if (paginationUrl.startsWith("/tags")) {
         return this.props.pageContext.tag;
       }
-      if(paginationUrl.startsWith("/page") && pageInfo.currentPage !== 1){
+      if (paginationUrl.startsWith("/page") && pageInfo.currentPage !== 1) {
         return "Page " + pageInfo.currentPage;
       }
       return "Blog";
@@ -41,20 +41,19 @@ class IndexPage extends React.Component{
             </div>
           )}
           {posts.map((post) => {
-            const node = post.node;
-            const tags = node.tags;
-            const { childMarkdownRemark } = node.body;
+            const tags = post.tags;
+            const { childMarkdownRemark } = post.body;
             const timeToRead = childMarkdownRemark.timeToRead;
             return (
-              <div id={node.id} key={node.id} className="d-block pb-3 blog-content">
+              <div id={post.id} key={post.id} className="d-block pb-3 blog-content">
                 <div className="post-container">
                   <div className="title posted">
-                    <Link to={`/blog/${node.slug}`} className="text-link">
-                      {node.title}
+                    <Link to={`/blog/${post.slug}`} className="text-link">
+                      {post.title}
                     </Link>
                   </div>
                   <div className="title text-info">
-                    <span className="page-info">{getPublishDate(node.publishDate)}</span>
+                    <span className="page-info">{getPublishDate(post.publishDate)}</span>
                     <span className="page-info">
                       {timeToRead} min{getPlurals(timeToRead)} read
                     </span>
@@ -62,8 +61,8 @@ class IndexPage extends React.Component{
                     <span className="page-info">{getPostTags(tags)}</span>
                   </div>
                   <div className="pt-1">
-                    {node.heroImage && (
-                      <GatsbyImage image={node.heroImage.gatsbyImageData} className="index-thumbnail" alt={"ferr"} />
+                    {post.heroImage && (
+                      <GatsbyImage image={post.heroImage.gatsbyImageData} className="index-thumbnail" alt={"ferr"} />
                     )}
                     <p>{childMarkdownRemark.excerpt}</p>
                   </div>
@@ -81,52 +80,44 @@ class IndexPage extends React.Component{
 }
 
 export const pageQuery = graphql`
-    query HomeQuery($skip: Int, $limit: Int, $tag: String) {
-        allContentfulBlogPost(
-            skip: $skip
-            limit: $limit
-            sort: { fields: publishDate, order: DESC }
-            filter: { tags: { eq: $tag } }
-        ) {
-            edges {
-                node {
-                    slug
-                    body {
-                        childMarkdownRemark {
-                            timeToRead
-                            excerpt(pruneLength: 365)
-                        }
-                    }
-                    tags
-                    title
-                    publishDate
-                    heroImage {
-                        gatsbyImageData(
-                            resizingBehavior: THUMB
-                            formats: WEBP
-                            cropFocus: FACES
-                            placeholder: BLURRED
-                            layout: FIXED
-                        )
-                    }
-                    id
-                }
-            }
-            pageInfo {
-                hasNextPage
-                hasPreviousPage
-                perPage
-                currentPage
-                pageCount
-                itemCount
-            }
+  query HomeQuery($skip: Int, $limit: Int, $tag: String) {
+    allContentfulBlogPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: publishDate, order: DESC }
+      filter: { tags: { eq: $tag } }
+    ) {
+      nodes {
+        slug
+        body {
+          childMarkdownRemark {
+            timeToRead
+            excerpt(pruneLength: 365)
+          }
         }
-        site {
-            siteMetadata {
-                siteUrl
-            }
+        tags
+        title
+        publishDate
+        heroImage {
+          gatsbyImageData(resizingBehavior: THUMB, formats: WEBP, cropFocus: FACES, placeholder: BLURRED, layout: FIXED)
         }
+        id
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        perPage
+        currentPage
+        pageCount
+        itemCount
+      }
     }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
 `;
 
 export default IndexPage;
