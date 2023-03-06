6315c4f06211764bae7f704a548d0631c132fe3a
ferr <ferry@advotics.com>
2023-03-06 10:34:11 +0700

    feat: change about me as anchor link for author name

--- src/components/sidebar/Bio.tsx
+++ src/components/sidebar/Bio.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./sidebar.css";
 import Photo from "../Photo";
 import { GatsbyImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 const Bio = ({ author, tagline }: BioAttr) => {
   const { gatsbyImageData: photo } = Photo();
   return (
     <div>
       <GatsbyImage image={photo} className="profile-img" alt="Ferry" title="Ferry Suhandri" />
-      <div className="mt-2 author-bio second-header">{author}</div>
+      <div className="mt-2 author-bio second-header">
+        <Link to={"/blog/berkenalan-dengan-ferry-suhandri"}>{author}</Link>
+      </div>
       <small className="muted">{tagline}</small>
     </div>
   );
