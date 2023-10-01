e9a9d67e574e5f7474e829325da3721f8fa3fe7e
MR Ferry <ferferfx@gmail.com>
2023-10-01 16:12:41 +0700

    test1

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,27 @@
  ************************/
 
 require("dotenv").config();
-const siteConfig = require("./config");
+// import siteConfig from "./config";
+const siteConfig = {
+  url: "https://ferry.vercel.app",
+  repo: "7sferry/gatsbyan",
+  title: "MR Ferry",
+  tagline: "An ISTJ, Type 5, Engineer, Gamer, and Thriller-Movies-Lover",
+  description: `Blog by Ferry Suhandri. An ISTJ, Type 5 Enneagram, & Software Engineer dari Solok. Berisi pandangan pribadi & pemrograman dalam Bahasa Indonesia`,
+  copyright: `© ${new Date().getFullYear()} Ferry Suhandri.`,
+  author: {
+    name: "Ferry S",
+    realName: "Ferry Suhandri",
+    contacts: {
+      linkedin: "https://www.linkedin.com/in/7sferry/",
+      github: "https://github.com/7sferry",
+      facebook: "https://facebook.com/7sferry",
+      crystal: "https://www.crystalknows.com/p/ferry",
+      stackOverFlow: "https://stackoverflow.com/users/14286378/ferry",
+      resume: "https://ferry.netlify.com/resume/",
+    },
+  },
+};
 
 function isProduction() {
   return process.env.NODE_ENV === "production" || !process.env.PREVIEW_TOKEN;
