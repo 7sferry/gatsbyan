1eacc7bdf7c3942334e6cea384240065d739904a
MR Ferry <ferferfx@gmail.com>
2023-10-01 15:58:28 +0700

    test

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -79,6 +79,7 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
       },
     },
     {
