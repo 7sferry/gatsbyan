db55e2eb8136735640520fc15487d1679d8efffa
ferry <7sferry@gmail.com>
2024-03-26 17:40:33 +0700

    test

--- src/utils/DateTimeUtils.tsx
+++ src/utils/DateTimeUtils.tsx
@@ -5,9 +5,10 @@ const defaultTimeZone = "Asia/Jakarta";
 
 export function formatToPattern(dateArg: string | Date, formatString: string, timeZone: string = defaultTimeZone) {
   const date = typeof dateArg === "string" ? new Date(dateArg) : dateArg;
-  let timeZoneInfo = findTimeZone(timeZone);
-  const utcOffset = getUTCOffset(date, timeZoneInfo);
-  const offsetDiff = utcOffset.offset - date.getTimezoneOffset();
-  const zonedDate = new Date(date.getTime() - offsetDiff * 60 * 1000);
+  // let timeZoneInfo = findTimeZone(timeZone);
+  // const { offset } = getUTCOffset(date, timeZoneInfo);
+  // const offsetDiff = offset - date.getTimezoneOffset();
+  // const zonedDate = new Date(date.getTime() - offsetDiff * 60 * 1000);
+  const zonedDate = date.toLocaleString("en-US", { timeZone: "Asia/Tokyo" });
   return format(zonedDate, formatString);
 }
