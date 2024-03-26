72764115895e9eab1768532be88240ac0a4d7e6c
ferry <7sferry@gmail.com>
2024-03-26 18:42:00 +0700

    feat: date utils

--- src/utils/DateTimeUtils.tsx
+++ src/utils/DateTimeUtils.tsx
@@ -1,13 +1,9 @@
-import { findTimeZone, getUTCOffset } from "timezone-support";
 import { format } from "date-fns";
 
 const defaultTimeZone = "Asia/Jakarta";
 
 export function formatToPattern(dateArg: string | Date, formatString: string, timeZone: string = defaultTimeZone) {
   const date = typeof dateArg === "string" ? new Date(dateArg) : dateArg;
-  let timeZoneInfo = findTimeZone(timeZone);
-  const utcOffset = getUTCOffset(date, timeZoneInfo);
-  const offsetDiff = utcOffset.offset - date.getTimezoneOffset();
-  const zonedDate = new Date(date.getTime() - offsetDiff * 60 * 1000);
+  const zonedDate = date.toLocaleString("en-US", { timeZone: timeZone });
   return format(zonedDate, formatString);
 }
