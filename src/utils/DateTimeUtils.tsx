ee9e563ec45bdcee580c301c3c269563ec598280
ferry <7sferry@gmail.com>
2024-03-26 18:35:39 +0700

    Revert "test"
    This reverts commit 322243935c24b782212b18b2924f614ea7abbe17.

--- src/utils/DateTimeUtils.tsx
+++ src/utils/DateTimeUtils.tsx
@@ -4,7 +4,8 @@ import { format } from "date-fns";
 const defaultTimeZone = "Asia/Jakarta";
 
 export function formatToPattern(dateArg: string | Date, formatString: string, timeZone: string = defaultTimeZone) {
-  const date = typeof dateArg === "string" ? new Date(dateArg) : dateArg;
-  const zonedDate = date.toLocaleString("en-US", { timeZone: timeZone });
+  const zonedDate = typeof dateArg === "string" ? new Date(dateArg) : dateArg;
+  // const zonedDate = date.toLocaleString("en-US", { timeZone: "Asia/Tokyo" });
+  console.log(zonedDate);
   return format(zonedDate, formatString);
 }
