6d6a27aa0904c346f5090762668b8f69a09189f4
MR Ferry <ferferfx@gmail.com>
2024-05-06 12:35:49 +0700

    test no js

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -202,6 +202,7 @@ const gatsbyConfig: GatsbyConfig = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     "gatsby-plugin-offline",
+    "gatsby-plugin-no-javascript",
     // {
     //   resolve: "gatsby-plugin-algolia",
     //   options: {
