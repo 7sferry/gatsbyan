95319717650c57bd09052d18a8cacd4cf034d92b
MR Ferry <ferferfx@gmail.com>
2024-05-06 13:02:11 +0700

    set head false on analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -180,7 +180,7 @@ const gatsbyConfig: GatsbyConfig = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          // head: true,
+          head: false,
           // Setting this parameter is also optional
           // respectDNT: true,
           // Avoids sending pageview hits from custom paths
