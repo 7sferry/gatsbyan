5c60e508d8c686aca32f00a56732d9e87757119b
MR Ferry <ferferfx@gmail.com>
2024-05-06 12:17:01 +0700

    Revert "test static image"
    This reverts commit 53a700dd0fda3955544b11ac86b140da11c3b103.

--- src/components/sidebar/Bio.tsx
+++ src/components/sidebar/Bio.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./sidebar.css";
-import { StaticImage } from "gatsby-plugin-image";
+import Photo from "../Photo";
+import { GatsbyImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
 import { BioAttr } from "../../types/DataTypes";
 
 const Bio = ({ author, tagline }: BioAttr) => {
-  // const { gatsbyImageData: photo } = Photo();
+  const { gatsbyImageData: photo } = Photo();
   return (
     <div>
-      <StaticImage src={"/ferry-suhandri.jpg"} className="profile-img" alt="Ferry" title="Ferry Suhandri" />
+      <GatsbyImage image={photo} className="profile-img" alt="Ferry" title="Ferry Suhandri" />
       <div className="mt-2 author-bio second-header">
         <Link to={"/blog/berkenalan-dengan-ferry-suhandri"}>{author}</Link>
       </div>
