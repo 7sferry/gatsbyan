53a700dd0fda3955544b11ac86b140da11c3b103
MR Ferry <ferferfx@gmail.com>
2024-05-06 11:46:48 +0700

    test static image

--- src/components/header/MobileBio.tsx
+++ src/components/header/MobileBio.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import Photo from "../Photo";
-import { GatsbyImage } from "gatsby-plugin-image";
 import "./header.css";
 import Socials from "../Socials";
 import { Link } from "gatsby";
 import { MobileBioAttr } from "../../types/DataTypes";
 
 const MobileBio = (props: MobileBioAttr) => {
-  const { gatsbyImageData: photo } = Photo();
+  // const { gatsbyImageData: photo } = Photo();
   return (
     <>
       <Socials mobile={true} contacts={props.contacts} />
       <div className="mobile-bio-main">
-        <GatsbyImage image={photo} className="bio-picture" alt="author-pic" />
+        {/*<GatsbyImage image={photo} className="bio-picture" alt="author-pic" />*/}
         <div className="bio-letter">
           <p className="bio-name second-header">
             <Link to={"/blog/berkenalan-dengan-ferry-suhandri"}>{props.author}</Link>
--- src/components/sidebar/Bio.tsx
+++ src/components/sidebar/Bio.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./sidebar.css";
-import Photo from "../Photo";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 import { Link } from "gatsby";
 import { BioAttr } from "../../types/DataTypes";
 
 const Bio = ({ author, tagline }: BioAttr) => {
-  const { gatsbyImageData: photo } = Photo();
+  // const { gatsbyImageData: photo } = Photo();
   return (
     <div>
-      <GatsbyImage image={photo} className="profile-img" alt="Ferry" title="Ferry Suhandri" />
+      <StaticImage src={"/ferry-suhandri.jpg"} className="profile-img" alt="Ferry" title="Ferry Suhandri" />
       <div className="mt-2 author-bio second-header">
         <Link to={"/blog/berkenalan-dengan-ferry-suhandri"}>{author}</Link>
       </div>
