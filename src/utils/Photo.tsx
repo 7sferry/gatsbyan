8ef2a946e52d278df49ac3529d43aa13373b7275
MR Ferry <ferferfx@gmail.com>
2024-05-19 22:48:15 +0700

    test

--- src/utils/Photo.tsx
+++ src/utils/Photo.tsx
@@ -4,18 +4,29 @@
  ************************/
 
 import { graphql, useStaticQuery } from "gatsby";
+import { withArtDirection } from "gatsby-plugin-image";
 
 const getPhotoBio = () => {
-  const { file } = useStaticQuery(graphql`
+  const { file, small } = useStaticQuery(graphql`
     query PhotoBio {
-      file(relativePath: { eq: "ferry.jpg" }) {
+      file: file(relativePath: { eq: "ferry.jpg" }) {
         childImageSharp {
           gatsbyImageData(quality: 100, placeholder: DOMINANT_COLOR, layout: CONSTRAINED)
         }
       }
+      small: file(relativePath: { eq: "ferry.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(quality: 100, placeholder: DOMINANT_COLOR, layout: CONSTRAINED, height: 75)
+        }
+      }
     }
   `);
-  return file.childImageSharp;
+  return withArtDirection(file?.childImageSharp?.gatsbyImageData, [
+    {
+      media: "(max-width: 1024px)",
+      image: small?.childImageSharp?.gatsbyImageData,
+    },
+  ]);
 };
 
 export default getPhotoBio;
