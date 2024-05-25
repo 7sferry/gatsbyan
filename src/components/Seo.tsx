ca64200afbd0d9a38a97856fde3eaea78c1e2f0b
MR Ferry <ferferfx@gmail.com>
2024-05-25 10:27:03 +0700

    test

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -31,18 +31,6 @@ export default function Seo({ description, lang = "id", title = "", image, path 
   return (
     <>
       <html lang={lang} />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-        media="print"
-      />
-      <noscript>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </noscript>
       <title>{`${title} ${name}`}</title>
       <meta name="description" content={metaDescription} />
       <meta name="og:title" content={title} />
