6aceb6ddc0bcdf065871d00a97cea1fccdabb6f6
MR Ferry <ferferfx@gmail.com>
2024-05-25 14:49:13 +0700

    refactor: remove unused font and css

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -14,46 +14,46 @@ export function onPreRenderHTML({ getHeadComponents, replaceHeadComponents }: an
 
 export const onRenderBody = ({ setHeadComponents }: any) => {
   setHeadComponents([
-    <link
-      key="work-sans-italic-vietnam"
-      rel="preload"
-      href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDkv_1w4A.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="work-sans-italic-latin-ext"
-      rel="preload"
-      href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDlv_1w4A.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="work-sans-italic-latin"
-      rel="preload"
-      href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDrv_0.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="work-sans-normal-vietnam"
-      rel="preload"
-      href="/fonts/QGYsz_wNahGAdqQ43Rh_c6Dpp_k.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="work-sans-normal-latin-ext"
-      rel="preload"
-      href="/fonts/QGYsz_wNahGAdqQ43Rh_cqDpp_k.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
+    // <link
+    //   key="work-sans-italic-vietnam"
+    //   rel="preload"
+    //   href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDkv_1w4A.woff2"
+    //   as="font"
+    //   type="font/woff2"
+    //   crossOrigin="anonymous"
+    // />,
+    // <link
+    //   key="work-sans-italic-latin-ext"
+    //   rel="preload"
+    //   href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDlv_1w4A.woff2"
+    //   as="font"
+    //   type="font/woff2"
+    //   crossOrigin="anonymous"
+    // />,
+    // <link
+    //   key="work-sans-italic-latin"
+    //   rel="preload"
+    //   href="/fonts/QGYqz_wNahGAdqQ43Rh_eZDrv_0.woff2"
+    //   as="font"
+    //   type="font/woff2"
+    //   crossOrigin="anonymous"
+    // />,
+    // <link
+    //   key="work-sans-normal-vietnam"
+    //   rel="preload"
+    //   href="/fonts/QGYsz_wNahGAdqQ43Rh_c6Dpp_k.woff2"
+    //   as="font"
+    //   type="font/woff2"
+    //   crossOrigin="anonymous"
+    // />,
+    // <link
+    //   key="work-sans-normal-latin-ext"
+    //   rel="preload"
+    //   href="/fonts/QGYsz_wNahGAdqQ43Rh_cqDpp_k.woff2"
+    //   as="font"
+    //   type="font/woff2"
+    //   crossOrigin="anonymous"
+    // />,
     <link
       key="work-sans-normal-latin"
       rel="preload"
