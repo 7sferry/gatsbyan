82a75f1fbedc9d231e2728519d028a2552c5848a
MR Ferry <ferferfx@gmail.com>
2024-11-17 22:45:35 +0700

    feat: move gtag to head

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -189,7 +189,7 @@ const gatsbyConfig: GatsbyConfig = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          // head: true,
+          head: true,
           // Setting this parameter is also optional
           // respectDNT: true,
           // Avoids sending pageview hits from custom paths
