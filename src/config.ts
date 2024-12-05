c0e783f83977e6854b2b287687f9ba18dc2d7dce
MR Ferry <ferferfx@gmail.com>
2024-12-05 17:57:36 +0700

    feat: tagline truncated only for mob

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const siteConfig: SiteConfig = {
   repo: "7sferry/gatsbyan",
   title: "MR Ferry",
   tagline: `An ISTJ of MBTI, C style of DiSC, and Type 5 of Enneagram. \nInterested in software engineering, money investing, gaming, football, psychology, and sharing POV about life & experiences.`,
-  description: `Software Engineer dari Solok. Membahas pemrograman, pengalaman hidup, dan pandangan pribadi`,
+  description: `Blog tentang Software Engineer dari Solok. Membahas pemrograman, investasi, game, sepak bola, psikologi, dan berbagi pengalaman serta pandangan pribadi`,
   copyright: `© ${new Date().getFullYear()} · Ferry Suhandri`,
   author: {
     name: "Ferry S",
